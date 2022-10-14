eeb9ee6662c1035041c6a0853fac73a5aad74c9f
Sanoop <sanoop.ps@codilar.com>
2022-10-15 01:36:57 +0530

    Removing unwanted code

--- src/resources/views/template/app.blade.php
+++ src/resources/views/template/app.blade.php
@@ -1,34 +1,5 @@
 @extends('bui-components::layout.app')
 @section('contents')
-    <div class="card">
-        <div class="card-header">
-            Form
-        </div>
-        <div class="card-body">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <div class="form-group form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                    <label class="form-check-label" for="exampleRadios1">
-                        Default radio
-                    </label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    </div>
     <x-buicomponents::ui.form method="POST" action="{{ route('test') }}">
             <x-buicomponents::ui.input type="text" name="test" label="Test" placeholder="Please Enter" data-id="taa" required>Sanoop</x-buicomponents::ui.input>
             <x-buicomponents::ui.checkbox name="checkbox" id="testc" value="1" required>Check me out</x-buicomponents::ui.checkbox>
